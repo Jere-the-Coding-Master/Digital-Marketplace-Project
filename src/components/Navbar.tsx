bf18119d51ce043cbae5ff7e5abb72257267072e
Jeremiah L. Manalang <jeremiah.manalang123@gmail.com>
2025-10-14 01:21:27 +0800

    NavBar Improved and More TypeScript

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
+import { Link } from "lucide-react"
+import { Icons } from "./Icons"
 import MaxWidthWrapper from "./MaxWidthWrapper"
+import NavItems from "./NavItems"
 
-const Navbar =() => {
+export const Navbar =() => {
     return (
         <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
             <header className="relative bg-white"> 
@@ -11,9 +14,14 @@ const Navbar =() => {
 
                             <div className="ml-4 flex lg:ml-0">
                                 <Link href='/'>
-                                    
+                                    <Icons.logo className='h-10 lg:ml-0'/>
                                 </Link>
                             </div>
+
+                            <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                                <NavItems />
+                            </div>
+
                         </div>
                     </div>
                 </MaxWidthWrapper>
---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits" as const,
+        featured:[
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: '/nav/ui_kits/mixed.jpg',
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: '/nav/ui_kits/blue.jpg',
+            },
+            {
+                name: "Best Sellers",
+                href: "#",
+                imageSrc: '/nav/ui_kits/purple.jpg',
+            },
+        ],
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured:[
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: '/nav/ui_kits/mixed.jpg',
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: '/nav/ui_kits/blue.jpg',
+            },
+            {
+                name: "Best Sellers",
+                href: "#",
+                imageSrc: '/nav/ui_kits/purple.jpg',
+            },
+        ],
+    },
+]
